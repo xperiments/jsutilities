95cee01d10ccae77c9448fdfff3e9f6765268e28
Corey Birnbaum <vonwolfehaus@gmail.com>
2013-01-24 10:42:56 -0800

    Added custom EventDispatcher.js; fixed extend.js
    Silly syntax errors in extend

---
+++ EventDispatcher.js
@@ -0,0 +1,109 @@
+var util = util || {};
+/**
+ * Very useful custom event dispatcher class. Based on the same by Nicholas Zakas.
+ * This version does not create objects after initialization, making it more performant and memory-friendly,
+ * suitable for games. I also added two helpful functions: removeListenersOfType and removeAllListeners.
+ * See github readme for example usage.
+ * 
+ * Original class by Zakas is at: http://www.nczonline.net/blog/2010/03/09/custom-events-in-javascript/
+ * 
+ * @author Corey Birnbaum, Nicholas Zakas
+ */
+util.EventDispatcher = function() {
+	
+	/**
+	 * @private
+	 * @this Object
+	 */
+	var _eventList = {};
+
+	/**
+	 * Excecutes the callback function that was defined by caller's addEventListener call.
+	 * @param {Object} e The event object that will be passed to the listener. Format should follow this convention:
+	 * 		e = { type:'load', target:_self, param2:'', ... }
+	 * @return {void}
+	 */
+	this.trigger = function (e) {
+		var type = e.type;
+		if (typeof type !== 'string' || typeof _eventList[type] === 'undefined') {
+			return;
+		}
+
+		var length = _eventList[type].length;
+		for (var i = 0; i < length; i++) {
+			if (typeof _eventList[type][i] != 'function') {
+				continue;
+			}
+			_eventList[type][i](e);
+		}
+	};
+
+	/**
+	 * 'addListener' is used to save some typing, but mostly to distinguish this class from the DOM variant.
+	 * @param {string} type 		Event tag.
+	 * @param {function} listener 	Callback to fire when this event type is triggered.
+	 */
+	this.addListener = function (type, listener) {
+		if (typeof _eventList[type] === 'undefined') {
+			_eventList[type] = [];
+		}
+		_eventList[type].push(listener);
+	};
+
+	/**
+	 * Removes a specific listener instance/callback.
+	 * @param {string} type 		Event tag.
+	 * @param {function} listener 	Callback that was associated with this event type.
+	 */
+	this.removeListener = function (type, listener) {
+		if (typeof _eventList[type] === 'undefined') {
+			return;
+		}
+		if (_eventList[type] instanceof Array){
+            var listeners = _eventList[type],
+            	i = 0, l = listeners.length;
+            while (i++ < l) {
+                if (listeners[i] === listener){
+                    listeners.splice(i, 1);
+                    break;
+                }
+            }
+        }
+	};
+	
+	/**
+	 * Removes all listeners of a particular type. Removes their reference but the type list
+	 * is merely reduced to 0 elements, so it doesn't have to create a new array if a listener
+	 * of type is added later.
+	 * To completely delete all listeners (AND types), use removeAllListeners.
+	 * @param {type} type Event tag to clear.
+	 */
+	this.removeListenersOfType = function (type) {
+		_eventList[type].length = 0;
+	};
+	
+	/**
+	 * Dangerous method! Nullifies all listeners and all types, leaving a completely empty event list.
+	 * This should only be run when the subclass is being cleaned up, at the end of its lifecycle.
+	 */
+	this.removeAllListeners = function () {
+		var type, typeList;
+		for (type in _eventList) {
+			_eventList[type] = null;
+			delete _eventList[type];
+		}
+	};
+	
+	/**
+	 * Test to see if an object has any listeners attached to a particular event type.
+	 * @param {string} type 	Event tag to clear.
+	 * @return {boolean}
+	 */
+	this.hasListener = function(type) {
+		return _eventList[type] != null;
+	};
+	
+	this.toString = function() {
+		return '[object EventDispatcher]';
+	};
+};
--- extend.js
+++ extend.js
@@ -1,137 +1,134 @@
 var util = util || {};
+/**
+ * jQuery.extend() without jQuery, placed in global scope with supporting/useful functions in the util namespace.
+ */
 (function() {
-/***************************************
- * jQuery.extend() without jQuery
- */
-
-/**
- * private support objects that i don't want hanging around in this namespace
- */
-var class2type = {},
-	listOfClasses = "Boolean Number String Function Array Date RegExp Object Error".split(" "),
-	core_toString = class2type.toString,
-	core_hasOwn = class2type.hasOwnProperty;
-var type = function( obj ) {
-	if ( obj == null ) {
-		return String( obj );
-	}
-	return typeof obj === "object" || typeof obj === "function" ?
-		class2type[ core_toString.call(obj) ] || "object" :
-		typeof obj;
-};
-var isEmptyObject = function( obj ) {
-	var name;
-	for ( name in obj ) {
-		return false;
-	}
-	return true;
-};
-
-// Populate the class2type map
-listOfClasses.forEach(function(name, i) {
-	class2type[ "[object " + name + "]" ] = name.toLowerCase();
-});
-/**
- * end privates
- */
-
-/**
- * Merge the contents of two or more objects together into the first object.
- * @see http://api.jquery.com/jQuery.extend/
- * @param [deep] 			If true, the merge becomes recursive (aka. deep copy). Default is TRUE.
- * @param target			An object that will receive the new properties if additional objects are passed in.
- * @param [object1..N] 		An object containing additional properties to merge in. Can list any additional objects containing properties to merge in.
- * @returns target 		The modified target object.
- */
-window.extend = function() {
-	var options, name, src, copy, copyIsArray, clone,
-		target = arguments[0] || {},
-		i = 1,
-		length = arguments.length,
-		deep = true;
-
-	// Handle a deep copy situation
-	if ( typeof target === "boolean" ) {
-		deep = target;
-		target = arguments[1] || {};
-		// skip the boolean and the target
-		i = 2;
-	}
-
-	// Handle case when target is a string or something (possible in deep copy)
-	if ( typeof target !== "object" && type(target) !== "function" ) {
-		target = {};
-	}
-
-	while (i < length) {
-		// Only deal with non-null/undefined values
-		if ( (options = arguments[ i ]) != null ) {
-			// Extend the base object
-			for ( name in options ) {
-				src = target[ name ];
-				copy = options[ name ];
-
-				// Prevent never-ending loop
-				if ( target === copy ) {
-					continue;
-				}
-
-				// Recurse if we're merging plain objects or arrays
-				if ( deep && copy && ( util.isPlainObject(copy) || (copyIsArray = Array.isArray(copy)) ) ) {
-					if ( copyIsArray ) {
-						copyIsArray = false;
-						clone = src && Array.isArray(src) ? src : [];
-
-					} else {
-						clone = src && util.isPlainObject(src) ? src : {};
-					}
-
-					// Never move original objects, clone them
-					target[ name ] = util.extend( deep, clone, copy );
-
-				// Don't bring in undefined values
-				} else if ( copy !== undefined ) {
-					target[ name ] = copy;
-				}
-			}
+	/****
+	 private support objects that i don't want hanging around in this namespace */
+	var class2type = {},
+		listOfClasses = "Boolean Number String Function Array Date RegExp Object Error".split(" "),
+		core_toString = class2type.toString,
+		core_hasOwn = class2type.hasOwnProperty;
+	var checkType = function( obj ) {
+		if ( obj == null ) {
+			return String( obj );
 		}
-		i++;
-	}
-
-	// Return the modified object
-	return target;
-};
-
-util.isWindow = function( obj ) {
-	return obj != null && obj == obj.window;
-};
-
-util.isNumeric = function( obj ) {
-	return !isNaN( parseFloat(obj) ) && isFinite( obj );
-};
-
-util.isPlainObject = function( obj ) {
-	// Not plain objects:
-	// - Any object or value whose internal [[Class]] property is not "[object Object]"
-	// - DOM nodes
-	// - window
-	if ( type( obj ) !== "object" || obj.nodeType || util.isWindow( obj ) ) {
-		return false;
-	}
-
-	// Support: Firefox >16
-	// The try/catch supresses exceptions thrown when attempting to access
-	// the "constructor" property of certain host objects, ie. |window.location|
-	try {
-		if ( obj.constructor &&
-				!core_hasOwn.call( obj.constructor.prototype, "isPrototypeOf" ) ) {
+		return typeof obj === "object" || typeof obj === "function" ?
+			class2type[ core_toString.call(obj) ] || "object" :
+			typeof obj;
+	};
+	var isEmptyObject = function( obj ) {
+		var name;
+		for ( name in obj ) {
 			return false;
 		}
-	} catch ( e ) {
-		return false;
-	}
+		return true;
+	};
+	listOfClasses.forEach(function(name, i) {
+		class2type[ "[object " + name + "]" ] = name.toLowerCase();
+	});
+	/* end privates
+	 ****/
 
-	// If the function hasn't returned already, we're confident that
-	// |obj| is a plain object, created by {} or constructed with new Object
-	return true;
-};
+
+	/**
+	 * Merge the contents of two or more objects together into the first object.
+	 * @see http://api.jquery.com/jQuery.extend/
+	 * @param [deep] 			If true, the merge becomes recursive (aka. deep copy). Default is TRUE.
+	 * @param target			An object that will receive the new properties if additional objects are passed in.
+	 * @param [object1..N] 		An object containing additional properties to merge in. Can list any additional objects containing properties to merge in.
+	 * @returns target 		The modified target object.
+	 */
+	window.extend = function() {
+		var options, name, src, copy, copyIsArray, clone,
+			target = arguments[0] || {},
+			i = 1,
+			length = arguments.length,
+			deep = true;
+
+		// Handle a deep copy situation
+		if ( typeof target === "boolean" ) {
+			deep = target;
+			target = arguments[1] || {};
+			// skip the boolean and the target
+			i = 2;
+		}
+
+		// Handle case when target is a string or something (possible in deep copy)
+		if ( typeof target !== "object" && checkType(target) !== "function" ) {
+			target = {};
+		}
+
+		while (i < length) {
+			// Only deal with non-null/undefined values
+			if ( (options = arguments[ i ]) != null ) {
+				// Extend the base object
+				for ( name in options ) {
+					src = target[ name ];
+					copy = options[ name ];
+
+					// Prevent never-ending loop
+					if ( target === copy ) {
+						continue;
+					}
+
+					// Recurse if we're merging plain objects or arrays
+					if ( deep && copy && ( util.isPlainObject(copy) || (copyIsArray = Array.isArray(copy)) ) ) {
+						if ( copyIsArray ) {
+							copyIsArray = false;
+							clone = src && Array.isArray(src) ? src : [];
+
+						} else {
+							clone = src && util.isPlainObject(src) ? src : {};
+						}
+
+						// Never move original objects, clone them
+						target[ name ] = window.extend( deep, clone, copy );
+
+					// Don't bring in undefined values
+					} else if ( copy !== undefined ) {
+						target[ name ] = copy;
+					}
+				}
+			}
+			i++;
+		}
+
+		// Return the modified object
+		return target;
+	};
+
+	util.isWindow = function( obj ) {
+		return obj != null && obj == obj.window;
+	};
+
+	util.isNumeric = function( obj ) {
+		return !isNaN( parseFloat(obj) ) && isFinite( obj );
+	};
+
+	util.isPlainObject = function( obj ) {
+		// Not plain objects:
+		// - Any object or value whose internal [[Class]] property is not "[object Object]"
+		// - DOM nodes
+		// - window
+		if ( checkType( obj ) !== "object" || obj.nodeType || util.isWindow( obj ) ) {
+			return false;
+		}
+
+		// Support: Firefox >16
+		// The try/catch supresses exceptions thrown when attempting to access
+		// the "constructor" property of certain host objects, ie. |window.location|
+		try {
+			if ( obj.constructor &&
+					!core_hasOwn.call( obj.constructor.prototype, "isPrototypeOf" ) ) {
+				return false;
+			}
+		} catch ( e ) {
+			return false;
+		}
+
+		// If the function hasn't returned already, we're confident that
+		// |obj| is a plain object, created by {} or constructed with new Object
+		return true;
+	};
+}());
