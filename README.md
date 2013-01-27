c853154ac454f73a9c371ed700fcb5b54fd109cc
Corey Birnbaum <vonwolfehaus@gmail.com>
2013-01-27 15:31:57 -0800

    added an efficient image loader

--- README.md
+++ README.md
@@ -1,10 +1,46 @@
 # Utilities
 
-## extend: jQuery.extend without the jQuery
+## util.extend: jQuery.extend without the jQuery
+
+## loader
+
+Tiny, efficient image loading class. No error checking yet tho. Or maybe you can add it?
+
+### Usage
+
+	window.addEventListener('load', function init() {
+		window.removeEventListener('load', init);
+		
+		// yup, it's just an object, not a function, so no need to call 'new'! BONUS
+		loader.addImage('img/thing.png');
+		loader.addImage('img/otherthing.png');
+		loader.addImage('img/ok.png');
+		
+		// optional
+		loader.onImageProgress = function(percent) {
+			console.log('[index.html] Image loading '+percent+'% complete');
+			// insert loading graphic update here or whatever
+		};
+		
+		loader.onImagesLoaded = function() {
+			console.log('[index.html] Images 100% loaded');
+			
+			// once complete you can retreive your resources through loader.images which is just an object
+			// with the filename as the key and the image itself as the value
+			coolObject.init(loader.images['coolObjectSprite']);
+		};
+		
+		// kick it off. note that you can simply add more in later and do this again without a problem
+		// so if you wanted to only download images on a per-level basis in your game, you can totally do that
+		loader.start();
+	});
+That's it. I like it.
 
 ## EventDispatcher.js
 
 Extremely useful class for building event-driven applications. This one is based on the design by [Nicholas Zakas](http://www.nczonline.net/blog/2010/03/09/custom-events-in-javascript/).
+
+TypeScript version is available as well. To use it, simply extend it and call this.trigger(eventObject). Otherwise it's exaclty the same.
 
 ### Usage
 
---
+++ loader.js
@@ -0,0 +1,101 @@
+/**
+ * A simple and efficient image loading mechanism with just enough features.
+ * The idea is to be able to load different types of assets, all within a single
+ * namespace ('loader'), in a sort of mixin way.
+ * There's no reason for this to be a class--this is much nicer to deal with as a global object.
+ * 
+ * @author Corey Birnbaum
+ */
+var loader = loader || {};
+(function() {
+
+/**
+ * Public object that stores loaded images as values, with their filename (sans extension) as keys.
+ * 
+ */
+loader.images = {};
+/**
+ * Callback that fires when all images are loaded--replace this property with an actual function.
+ * Launch your app within this callback, then pick up your resources according to filename from
+ * loader.images at any time in any context.
+ * It will still pass in loader.images as the sole parameter tho, in case that's your thing.
+ */
+loader.onImagesLoaded = null;
+/**
+ * Callback that fires when an image is loaded. Used for loading bars.
+ * It passes in a normalized number according to loaded/total, 0 - 1.
+ */
+loader.onImageProgress = null;
+/**
+ * TODO
+ * Set this to the number of resources you want to process in parallel.
+ */
+// loader.runParallel = 1;
+
+// @private
+var _loaded = 0, _total = 0, _queue = [];
+
+/**
+ * Add an image to load by passing in its URL.
+ * @param url The location of the resource to load.
+ */
+loader.addImage = function(url) {
+	_queue.push(url);
+	_total++;
+};
+
+/**
+ * Once images have been added to the queue, kick off the load process to download
+ * each in sequence.
+ */
+loader.start = function() {
+	_loaded = 0;
+	if (_total > 0) {
+		_queue.reverse(); // reversed so we can pop elements in order, which is safer/cheaper than shifting
+		processQueue();
+	}
+};
+
+/**
+ * Creates the actual image objects and sets them up for loading.
+ * @private
+ */
+var processQueue = function() {
+	var nextURL = _queue.pop(),
+		img = new Image(),
+		tag = nextURL.split('/');
+	
+	tag = tag[tag.length-1]; // file name
+	tag = tag.split('.')[0]; // name without extension
+	img.tag = tag;
+	// console.log('loading image '+img.tag);
+	
+	img.addEventListener('load', onImageComplete, false);
+	// kick off the download
+	img.src = nextURL;
+};
+
+/**
+ * Cleans up images and checks conditions.
+ * @private
+ */
+var onImageComplete = function(evt) {
+	_loaded++;
+	// console.log('image '+_loaded+' loaded');
+	
+	this.removeEventListener('load', onImageComplete);
+	loader.images[this.tag] = this;
+	
+	if (_loaded == _total && typeof loader.onImagesLoaded === 'function') {
+		_total = 0; // in case there's a next time
+		loader.onImagesLoaded(loader.images);
+	} else {
+		processQueue();
+		if (typeof loader.onImageProgress === 'function') {
+			// 0-1 at 1/100th decimal point, eg 0.34, 0.66, etc
+			var percentage = parseInt(_loaded/_total*100)/100;
+			loader.onImageProgress(percentage);
+		}
+	}
+};
+}());
