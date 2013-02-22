07adeb16b552f4dfe1ecdd5d3215fa09c6a28697
Corey Birnbaum <vonwolfehaus@gmail.com>
2013-02-22 10:16:00 -0800

    lots of cleanup; trimmed namespace

--- cssutils.js
+++ cssutils.js
@@ -1,8 +1,11 @@
-var bit = bit || {};
-bit.util = bit.util || {};
-
-bit.util.hasStyle = function(doc, className) {
-    console.log('[bit.util.hasStyle] Checking for a style...');
+var util = util || {};
+/**
+ * Check if a style exists in the document passed.
+ * @param {type} doc 		The document to check.
+ * @param {type} className 	The exact match of the rule to check for. eg `#thing` will not match `#thing .subthing`.
+ */
+util.hasStyle = function(doc, className) {
+    // console.log('[util.hasStyle] Checking for a style...');
     var s, i, x, classes,
     	l = doc.styleSheets.length;
     for (i = 0; i < l; i++) {
@@ -15,19 +18,24 @@ bit.util.hasStyle = function(doc, className) {
 	    }
     }
     return false;
-}
+};
 
-bit.util.insertRule = function(doc, rule) {
+/**
+ * Append a rule to the stylesheet in the passed document. If a stylesheet doesn't exist, one will be created.
+ * @param {type} doc  	The document to append the rule to.
+ * @param {type} rule 	The full rule to add, eg `.my-class { display:inline-block; margin:0px; }`.
+ */
+util.insertRule = function(doc, rule) {
 	var sliceTo = rule.indexOf('{');
 	var styleRule = rule.slice(0, sliceTo-1);
 	// don't duplicate styles
-	if (bit.util.hasStyle(doc, styleRule)) {
-		console.log('[bit.util.insertRule] That document already has that rule');
+	if (util.hasStyle(doc, styleRule)) {
+		console.log('[util.insertRule] That document already has that rule');
 		return;
 	}
-	console.log('[bit.util.insertRule] Inserting '+rule);
+	// console.log('[util.insertRule] Inserting '+rule);
 	if (doc.styleSheets.length == 0) {
-		console.log('[bit.util.insertRule] Creating style element');
+		// console.log('[util.insertRule] Creating style element');
 		var style = doc.createElement('style');
 		style.type = 'text/css';
 		var head = doc.getElementsByTagName('head')[0];
@@ -36,17 +44,11 @@ bit.util.insertRule = function(doc, rule) {
 		var styleTags = head.getElementsByTagName('style');
 		console.log(styleTags.length);
 	}
-	console.log('[bit.util.insertRule] Style elements: '+doc.styleSheets.length);
 	
 	var head = doc.getElementsByTagName('head')[0];
 	var styleTags = head.getElementsByTagName('style');
-	console.log(styleTags);
-	console.log(styleTags.length); // <--- SHOULD BE 1
-	// console.log(doc.styleSheets);
 	
 	var styleSheet = doc.styleSheets[doc.styleSheets.length-1];
 	var rules = styleSheet.cssRules || styleSheet.rules;
-	console.log('[bit.util.insertRule] rules '+rules);
 	styleSheet.insertRule(rule, rules.length);
-	console.log('[bit.util.insertRule] Injection complete ');
-}
+};
